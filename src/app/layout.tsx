b10ae45ece711b75b460c7eeaa7928b4bb6860ec
aayushi bhandari <aayushib.ug23.cs@nitp.ac.in>
2025-03-31 07:49:26 +0530

    vercel error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import { ORIGIN_URL } from "@/utils/helpers";
 const fontSans = FontSans({
   variable: "--font-sans",
   subsets: ["latin"],
@@ -14,6 +15,10 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   title: "PDFreakout",
   description: "PDFreakout is a AI SAAS application that summarize the pdf documents",
+  metadataBase : new URL(ORIGIN_URL),
+  alternates :{
+    canonical : ORIGIN_URL
+  }
 };
 
 export default function RootLayout({
